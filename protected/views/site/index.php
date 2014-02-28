29373c64590a6bd72cbe629bc070a049da4573a9
arossokha <arossokha@takeforce.com>
2014-02-28 14:37:49 +0200

    Css main page
    New block for calculating

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -6,6 +6,20 @@ $this->pageTitle=Yii::app()->name;
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<center>
-<img src="http://bestvalentinesday2014.com/wp-content/uploads/2014/02/valentines-day-wallpapers-saying-i-love-you-2014.jpg" width="500px">
-</center>
+<div class="calc">
+	Choose product <select>
+		<option disabled selected style='display:none;'>Select...</option>
+		<option>test</option>
+		<option>test2</option>
+	</select>
+
+	<div class="processor invisible">
+		<p>Calculating...</p>
+		<p>Materials		$<span id="materials-price"></span></p>
+		<p>Equip		$<span id="equip-price"></span></p>
+		<p>Work 		$<span id="work-price"></span></p>
+		<p>Other		$<span id="other-price"></span></p>
+		<hr>
+		<p>Summary		$<span id="summary-price"></span></p>
+	</div>	
+</div>
